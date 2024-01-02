a4784c8902fa8a3c8741a61711eafb4f9d8c3fe3
Navin Avadhani <knavinavadhani@gmail.com>
2024-01-02 21:35:35 +0530

    Added login functionality.

--- actions/login.ts
+++ actions/login.ts
@@ -1,11 +1,34 @@
 "use server";
 
 import { z } from "zod";
+
+import { signIn } from "~/auth";
 import { loginSchema } from "~/schemas";
+import { DEFAULT_LOGIN_REDIRECT } from "~/routes";
+import { AuthError } from "next-auth";
 
 export async function login(values: z.infer<typeof loginSchema>) {
   const validatedFields = loginSchema.safeParse(values);
   if (!validatedFields.success) return { error: "Invalid fields" };
+
+  const { email, password } = validatedFields.data;
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { error: "Invalid credentials" };
+        default:
+          return { error: "Something went wrong" };
+      }
+    }
+    throw error;
+  }
 
   return { success: "Email sent!" };
 }
--- auth.config.ts
+++ auth.config.ts
@@ -1,6 +1,25 @@
-import GitHub from "next-auth/providers/github";
+import { compare } from "bcryptjs";
 import type { NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+
+import { loginSchema } from "./schemas";
+import { getUserByEmail } from "./utils/users";
 
 export default {
-  providers: [GitHub],
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const validatedFields = loginSchema.safeParse(credentials);
+
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
+          const user = await getUserByEmail(email);
+          if (!user || !user.password) return null;
+          const doPasswordsMatch = await compare(password, user.password);
+          if (doPasswordsMatch) return user;
+        }
+        return null;
+      },
+    }),
+  ],
 } satisfies NextAuthConfig;
